1692375d4e2d90422341b359d2afcb896d6b582d
drfdev <drafff@gmail.com>
2024-01-01 11:39:12 +0400

    is OK build

---
+++ programming-structures/src/test/java/dev/drf/prog/IsOkTest.java
@@ -0,0 +1,13 @@
+package dev.drf.prog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IsOkTest {
+    @Test
+    void isOkTest() {
+        System.out.println(System.getProperty("java.version"));
+        assertTrue(true);
+    }
+}
